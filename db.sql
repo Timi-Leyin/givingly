1d203ce07d35d1d061e5e97afbf32355ce0b126c
Original Timi <timileyinoyelekan11@gmail.com>
2024-06-27 09:49:40 -0700

    feat: sql queries

---
+++ db.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `givingly` ;
+USE givingly;
+
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
